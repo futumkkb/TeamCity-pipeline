f022e16d00c6c8c5c575218b7adf8d2ceab710c1
André Rodrigues <mrnexusrider@hotmail.com>
2024-10-15 12:22:32 -0300

    Configuração inicial projeto springboot

---
+++ src/main/java/com/example/pipeline/demo/TeamCityPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.pipeline.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCityPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCityPipelineApplication.class, args);
+	}
+
+}
